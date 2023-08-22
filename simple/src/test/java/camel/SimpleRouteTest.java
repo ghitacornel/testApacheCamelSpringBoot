3300e8d59455df0c86f8744e718c55ac98926b0a
ghita <ghita.cornel@gmail.com>
2023-08-23 00:27:08 +0300

    better

--- simple/src/test/java/camel/SimpleRouteTest.java
+++ simple/src/test/java/camel/SimpleRouteTest.java
@@ -3,6 +3,8 @@ package camel;
 import camel.route.SimpleRoute;
 import lombok.SneakyThrows;
 import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.AdviceWith;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.Test;
@@ -18,13 +20,18 @@ public class SimpleRouteTest extends CamelTestSupport {
     @Test
     public void testRoute() {
 
-//        MockEndpoint mock = getMockEndpoint("bean:finalStep");
-//        mock.expectedBodiesReceived("expected message");
+        // advice the start route using the inlined AdviceWith lambda style route builder
+        // which has extended capabilities than the regular route builder
+        // mock all endpoints
+        AdviceWith.adviceWith(context, "simple-route", AdviceWithRouteBuilder::mockEndpoints);
+
+        getMockEndpoint("mock:direct:start").expectedBodiesReceived("Hello");
+//        getMockEndpoint("mock:bean:initialStep").expectedBodiesReceived("Hello");
+//        getMockEndpoint("mock:bean:step1").expectedBodiesReceived("Hello");
 
         String input = "Hello";
         template.sendBody("direct:start", input);
 
-//        mock.assertIsSatisfied();
-
+        MockEndpoint.assertIsSatisfied(context);
     }
 }
