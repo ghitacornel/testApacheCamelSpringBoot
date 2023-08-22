7d08bc033a4a55b2a968e7dce6e84022cedbf3d8
ghita <ghita.cornel@gmail.com>
2023-08-22 23:14:46 +0300

    better

--- simple/src/main/java/camel/route/steps/Step1.java
+++ simple/src/main/java/camel/route/steps/Step1.java
@@ -2,14 +2,14 @@ package camel.route.steps;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.springframework.stereotype.Component;
 
-@Component
-public class Step3 implements Processor {
+import java.util.Date;
+
+public class Step1 implements Processor {
     @Override
     public void process(Exchange exchange) {
-        Long bodyLong = exchange.getIn().getBody(Long.class);
-        String body = bodyLong + " 3";
+        Date bodyDate = exchange.getIn().getBody(Date.class);
+        Long body = bodyDate.getTime() + 222;
         exchange.getMessage().setBody(body);
     }
 }
