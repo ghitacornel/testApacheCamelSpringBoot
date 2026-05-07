e989be5168305d29ae0f90e2a923408542bdabdd
cornel <ghita.cornel@gmail.com>
2026-05-07 16:07:39 +0300

    better

---
+++ rest-rest-yaml/src/main/java/org/example/SpringBootMainRestRestYaml.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMainRestRestYaml {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMainRestRestYaml.class, args);
+    }
+}
