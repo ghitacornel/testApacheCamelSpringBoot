69b21bab5457915456721c3042b0a08e2fb3092c
cornel <ghita.cornel@gmail.com>
2026-05-07 18:03:37 +0300

    better

---
+++ rest-rest-yaml/src/main/java/org/example/model/ResponseDTO.java
@@ -0,0 +1,19 @@
+package org.example.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseDTO {
+
+    private String id;
+    private String name;
+
+}
--- rest-rest-yaml/src/test/java/org/example/RestRestTest.java
+++ rest-rest-yaml/src/test/java/org/example/RestRestTest.java
@@ -1,18 +1,25 @@
 package org.example;
 
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.example.model.RequestDTO;
+import org.example.model.ResponseDTO;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClient;
+import tools.jackson.databind.ObjectMapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @CamelSpringBootTest
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RestRestTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     // Spring will inject the random port assigned to the web server
     @LocalServerPort
@@ -59,13 +66,16 @@ public class RestRestTest {
     @Test
     public void testPost() {
 
-        ResponseEntity<String> response = restClient.post()
+        RequestDTO request = RequestDTO.builder().name("John").build();
+        ResponseDTO response = ResponseDTO.builder().name("John").build();
+
+        ResponseEntity<String> responseEntity = restClient.post()
                 .uri("http://localhost:" + webServerPort + "/camel/say/submit")
-                .body("{\"name\":\"ion\"}")
+                .body(request)
                 .retrieve().toEntity(String.class);
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isEqualTo("{\"name\":\"ion\"}");
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isEqualTo(objectMapper.writeValueAsString(response));
 
     }
 
