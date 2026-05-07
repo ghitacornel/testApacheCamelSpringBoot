6a7d8aac0bfc426ffe3ba2f5c46be42fb342e7e6
cornel <ghita.cornel@gmail.com>
2026-05-07 18:24:54 +0300

    better

---
+++ rest-rest-yaml/src/main/java/org/example/beans/RequestResponseProcessor.java
@@ -0,0 +1,20 @@
+package org.example.beans;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.example.model.RequestDTO;
+import org.example.model.ResponseDTO;
+
+public class RequestResponseProcessor implements Processor {
+
+    @Override
+    public void process(Exchange exchange) {
+        RequestDTO request = exchange.getIn().getBody(RequestDTO.class);
+        ResponseDTO response = ResponseDTO.builder()
+                .id("1")
+                .name(request.getName())
+                .build();
+        exchange.getMessage().setBody(response);
+    }
+
+}
--- rest-rest-yaml/src/test/java/org/example/RestRestTest.java
+++ rest-rest-yaml/src/test/java/org/example/RestRestTest.java
@@ -4,22 +4,17 @@ import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.example.model.RequestDTO;
 import org.example.model.ResponseDTO;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClient;
-import tools.jackson.databind.ObjectMapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @CamelSpringBootTest
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RestRestTest {
-
-    @Autowired
-    ObjectMapper objectMapper;
 
     // Spring will inject the random port assigned to the web server
     @LocalServerPort
@@ -67,15 +62,15 @@ public class RestRestTest {
     public void testPost() {
 
         RequestDTO request = RequestDTO.builder().name("John").build();
-        ResponseDTO response = ResponseDTO.builder().name("John").build();
+        ResponseDTO response = ResponseDTO.builder().id("1").name("John").build();
 
-        ResponseEntity<String> responseEntity = restClient.post()
+        ResponseEntity<ResponseDTO> responseEntity = restClient.post()
                 .uri("http://localhost:" + webServerPort + "/camel/say/submit")
                 .body(request)
-                .retrieve().toEntity(String.class);
+                .retrieve().toEntity(ResponseDTO.class);
 
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isEqualTo(objectMapper.writeValueAsString(response));
+        assertThat(responseEntity.getBody()).isEqualTo(response);
 
     }
 
