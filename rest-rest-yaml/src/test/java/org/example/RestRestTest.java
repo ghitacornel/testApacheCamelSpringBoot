50926c205f4476a7dd17d27cc1575a5683e9c888
cornel <ghita.cornel@gmail.com>
2026-05-07 16:46:38 +0300

    better

---
+++ rest-rest-yaml/src/test/java/org/example/RestRestTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@CamelSpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RestRestTest {
+
+    // Spring will inject the random port assigned to the web server
+    @LocalServerPort
+    int webServerPort;
+
+    RestClient restClient = RestClient.create();
+
+    @Test
+    public void testGet() {
+        ResponseEntity<String> response = restClient.get()
+                .uri("http://localhost:" + webServerPort + "/camel/say/hello")
+                .retrieve().toEntity(String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo("<html><body>Hello World</body></html>");
+    }
+
+}
